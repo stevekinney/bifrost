f6d157bc31bb9bd75490e0f0743ee4d1518f5242
Steve Kinney <hello@stevekinney.net>
2020-04-24 14:39:38 -0600

    Commit whatever it is that I was working on for Harrison

--- applications/email/index.js
+++ applications/email/index.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import { Summon } from 'bifrost';
 
 const Application = () => (
-  <main>
-    <h1>Twilio Email</h1>
-    <ul>
-      <li>
-        <a href="/email">Email</a>
-      </li>
-      <li>
-        <a href="/flex">Flex</a>
-      </li>
-      <li>
-        <a href="/voice">Voice</a>
-      </li>
-      <li>
-        <a href="/messaging">Messaging</a>
-      </li>
-      <li>
-        <a href="https://google.com">Google (External Link)</a>
-      </li>
-    </ul>
-  </main>
+  <Router>
+    <main>
+      <h1>Twilio Email</h1>
+      <ul>
+        <li>
+          <a href="/email">Email</a>
+        </li>
+        <li>
+          <a href="/flex">Flex</a>
+        </li>
+        <li>
+          <a href="/voice">Voice</a>
+        </li>
+        <li>
+          <a href="/messaging">Messaging</a>
+        </li>
+        <li>
+          <a href="https://google.com">Google (External Link)</a>
+        </li>
+        <li>
+          <Link to="/email/internal">Internal</Link>
+        </li>
+      </ul>
+      <Route path="/email/internal">
+        <h2>Internal</h2>
+        <p>Internal Linking Works</p>
+      </Route>
+    </main>
+  </Router>
 );
 
 render(
