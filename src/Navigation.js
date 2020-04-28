db2cf1b52d71da915d5287cd593d9e2a0165a6da
hharnisch <hharnisch@twilio.com>
2020-04-28 17:11:54 -0600

    add more pages and move to new key

--- src/Navigation.js
+++ src/Navigation.js
@@ -17,16 +17,28 @@ export const Navigation = () => (
         <NavLink to="/messaging">Messaging</NavLink>
       </li>
       <li>
-        <NavLink to="/console/phone-numbers/incoming?iframe=true">Numbers</NavLink>
+        <NavLink to="/console/phone-numbers/incoming?disable_navigation=true">Numbers</NavLink>
       </li>
       <li>
-        <NavLink to="/console/phone-numbers/getting-started?iframe=true">Numbers - Getting Started</NavLink>
+        <NavLink to="/console/phone-numbers/getting-started?disable_navigation=true">Numbers - Getting Started</NavLink>
       </li>
       <li>
-        <NavLink to="/console/phone-numbers/overview?iframe=true">Numbers - Overview</NavLink>
+        <NavLink to="/console/phone-numbers/overview?disable_navigation=true">Numbers - Overview</NavLink>
       </li>
       <li>
-        <NavLink to="/console/chat/dashboard?iframe=true">Chat</NavLink>
+        <NavLink to="/console/chat/dashboard?disable_navigation=true">Chat</NavLink>
+      </li>
+      <li>
+        <NavLink to="/console/functions/manage?disable_navigation=true">Functions</NavLink>
+      </li>
+      <li>
+        <NavLink to="/console/functions/api?disable_navigation=true">Functions - API</NavLink>
+      </li>
+      <li>
+        <NavLink to="/console/debugger?disable_navigation=true">Debugger</NavLink>
+      </li>
+      <li>
+        <NavLink to="/console/debugger/alert-triggers?disable_navigation=true">Debugger - Alert Triggers</NavLink>
       </li>
     </ul>
   </section>
